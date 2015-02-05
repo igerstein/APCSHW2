4806c73749d8bb9d47d545d78f58d05c9c543191
igerstein <isaac.gerstein1@gmail.com>
2015-02-05 17:40:40 -0500

    Added factorial method

--- 01recursion/Recursion.java
+++ 01recursion/Recursion.java
@@ -2,4 +2,13 @@ public class Recursion{
     public String name(){
 	return "Gerstein,Isaac";
     }
+    public int fact(int n){
+	if (n < 0){
+	    throw new IllegalArgumentException();
+	} else if (n < 2){
+	    return 1;
+	} else {
+	    return n * fact(n - 1);
+	}
+    }
 }
