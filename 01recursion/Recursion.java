faefa55fc379d369bbd61c895655086c860b5885
igerstein <isaac.gerstein1@gmail.com>
2015-02-05 18:20:38 -0500

    Added fib method with fibHelper

--- 01recursion/Recursion.java
+++ 01recursion/Recursion.java
@@ -11,4 +11,22 @@ public class Recursion{
 	    return n * fact(n - 1);
 	}
     }
+    public int fib(int n){
+	if (n < 0){
+	    throw new IllegalArgumentException();
+	} else if (n == 0){
+	    return 0;
+	} else if (n == 1){
+	    return 1;
+	} else {
+	    return fibHelper(n, 0, 1);
+	}
+    }
+    public int fibHelper(int n, int a, int b){
+	if (n == 0){
+	    return a;
+	} else {
+	    return fibHelper(n - 1, b, a + b);
+	}
+    }
 }
