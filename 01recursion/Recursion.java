7d45701e953fc0fbadef6419727b868a3f205473
igerstein <isaac.gerstein1@gmail.com>
2015-02-05 19:27:54 -0500

    Added sqrt method with sqrtHelper

--- 01recursion/Recursion.java
+++ 01recursion/Recursion.java
@@ -29,4 +29,21 @@ public class Recursion{
 	    return fibHelper(n - 1, b, a + b);
 	}
     }
+    public double sqrt(double n){
+	if (n < 0){
+	    throw new IllegalArgumentException();
+	} else if (n == 0){
+	    return 0.0;
+	} else {
+	    return sqrtHelper(n, 1.0);
+	}
+    }
+    public double sqrtHelper(double n, double guess){
+	double newGuess = (n / guess + guess) / 2;
+	if ((guess >= newGuess && guess / newGuess < 1.001) || (newGuess > guess && newGuess / guess < 1.001)){
+	    return newGuess;
+	} else {
+	    return sqrtHelper(n, newGuess);
+	}
+    }
 }
