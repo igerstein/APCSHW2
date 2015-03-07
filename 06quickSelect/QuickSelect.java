01b7d6974621aacdd34f96c747df58aaf1594c07
igerstein <isaac.gerstein1@gmail.com>
2015-03-06 19:11:19 -0500

    Added in-place partition method

--- 06quickSelect/QuickSelect.java
+++ 06quickSelect/QuickSelect.java
@@ -3,8 +3,9 @@ public class QuickSelect{
     public static void main(String[]args){
 	int[] ary = {6, 5, 2, 8, 7, 1, 4, 9, 0, 3};
 	QuickSelect a = new QuickSelect();
-	a.partition(ary, 0, 9);
+	a.partition2(ary, 0, 9);
     }
+    
     public void partition(int[] ary, int si, int ei){
 	int[] d = new int[ary.length];
 	for (int i = 0; i < d.length; i++){
@@ -30,6 +31,28 @@ public class QuickSelect{
 	d[si] = pivot;
 	for (int i = 0; i < d.length; i++){
 	    ary[i] = d[i];
+	}
+    }
+    
+    public void partition2(int[] ary, int si, int ei){
+	int start = si;
+	int end = ei;
+	int randomIndex = (int)(Math.random() * (ei - si + 1)) + si;
+	int pivot = ary[randomIndex];
+	int temp;
+	while (si != ei){
+	    if (ary[si] > pivot){
+		temp = ary[ei];
+		ary[ei] = ary[si];
+		ary[si] = temp;
+		ei--;
+	    }else if (ary[si] == pivot){
+		temp = ary[si + 1];
+		ary[si + 1] = ary[si];
+		ary[si] = temp;
+	    }else{
+		si++;
+	    }
 	}
     }
 }
