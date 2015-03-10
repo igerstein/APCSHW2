c08f6a89f90609eec7a77f21e4513861f0abe580
igerstein <isaac.gerstein1@gmail.com>
2015-03-10 18:17:57 -0400

    LNode class

---
+++ 07LNode/LNode.java
@@ -0,0 +1,21 @@
+public class LNode{
+    private int value;
+    private LNode next;
+
+    public int getValue(){
+	return value;
+    }
+
+    public LNode getNext(){
+	return next;
+    }
+
+    public void setValue(int value){
+	this.value = value;
+    }
+
+    public void setNext(LNode next){
+	this.next = next;
+    }
+}
+    
