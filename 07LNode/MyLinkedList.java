3287ffb3837af174a6cae4dd646269de7507a73e
igerstein <isaac.gerstein1@gmail.com>
2015-03-12 13:08:13 -0400

    Added add method

--- 07LNode/MyLinkedList.java
+++ 07LNode/MyLinkedList.java
@@ -1,6 +1,5 @@
 public class MyLinkedList{
     private LNode head;
-    private LNode current;
 
     public MyLinkedList(){
     }
@@ -8,35 +7,59 @@ public class MyLinkedList{
     public MyLinkedList(int size){
     }
 
+    public static void main(String[]args){
+	MyLinkedList a = new MyLinkedList();
+	a.add(1);
+	a.add(2);
+	a.add(3);
+	System.out.println(a);
+    }
+
     public String toString(){
 	String ans = "[ ";
 	LNode temp = head;
-	while (temp.getValue() != null){
+	while (temp != null){
 	    ans += temp.getValue() + ",";
-	    current = current.getNext();
+	    temp = temp.getNext();
 	}
 	return ans.substring(0, ans.length() - 1) + " ]";
     }
 
     public int get(int index){
+	return 0;
     }
 
     public void set(int index, int value){
     }
 
-    public void add(int value){
-	//First set head, then set at the end
+    public boolean add(int value){
+	if (head == null){
+	    head = new LNode();
+	    head.setValue(value);
+	}else{
+	    LNode temp = head;
+	    while (temp.getNext() != null){
+		temp = temp.getNext();
+	    }
+	    LNode nextNode = new LNode();
+	    nextNode.setValue(value);
+	    temp.setNext(nextNode);
+	}
+	return true;
     }
 
     public void add(int index, int value){
     }
 
     public int remove(int index){
+	return 0;
     }
 
     public int size(){
+	return 0;
     }
 
     public int indexOf(int value){
+	return 0;
     }
 }
