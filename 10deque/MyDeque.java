528e1a2c6dea2307c91c3cd76737e225b23cee19
igerstein <isaac.gerstein1@gmail.com>
2015-03-25 12:52:11 -0400

    Instance variables for deque

---
+++ 10deque/MyDeque.java
@@ -0,0 +1,11 @@
+public class MyDeque<T>{
+
+    public String name(){
+	return "gerstein.isaac";
+    }
+
+    private Object[] deque;
+    private int head = 0;
+    private int tail = 0;
+}
+    
