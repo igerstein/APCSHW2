77a3916b123d6639550579e03676ca3cd7f5dcba
igerstein <isaac.gerstein1@gmail.com>
2015-03-27 14:54:42 -0400

    addFirst method

--- 10deque/MyDeque.java
+++ 10deque/MyDeque.java
@@ -2,9 +2,12 @@ public class MyDeque<T>{
 
     public static void main(String[]args){
 	MyDeque<Integer> a = new MyDeque<Integer>(false);
-	a.addLast(new Integer(1));
-	a.addLast(new Integer(2));
-	a.addLast(new Integer(3));
+	a.addFirst(new Integer(1));
+	a.addFirst(new Integer(2));
+	a.addFirst(new Integer(3));
+	a.addLast(new Integer(4));
+	a.addLast(new Integer(5));
+	a.addFirst(new Integer(6));
 	System.out.println(a);
 	System.out.println(a.head);
 	System.out.println(a.tail);
@@ -57,6 +60,10 @@ public class MyDeque<T>{
 		tail = 0;
 	    }
 	    if (head == tail){
+		tail--;
+		if (tail == -1){
+		    tail = deque.length - 1;
+		}
 		resize(size * 2);
 		head = 0;
 		tail = size;
@@ -66,20 +73,42 @@ public class MyDeque<T>{
 	size++;
     }
 
+    public void addFirst(T value){
+	if (size == 0){
+	    deque = new Object[1];
+	    deque[0] = value;
+	    head = 0;
+	    tail = 0;
+	}else{
+	    head--;
+	    if (head == -1){
+		head = deque.length - 1;
+	    }
+	    if (head == tail){
+		head++;
+		if (head == deque.length){
+		    head = 0;
+		}
+		resize(size * 2);
+		head = deque.length - 1;
+		tail = size - 1;
+	    }
+	    deque[head] = value;
+	}
+	size++;
+    }
+
     public void resize(int newSize){
 	Object[] newDeque = new Object[newSize];
 	int c = 0;
-	if (tail <= head){
+	if (tail < head){
 	    for (int i = head; i < deque.length; i++){
 		newDeque[c] = deque[i];
 		c++;
 	    }
-	    for (int i = 0; i < tail; i++){
+	    for (int i = 0; i <= tail; i++){
 		newDeque[c] = deque[i];
 		c++;
-	    }
-	    if (tail != head){
-		newDeque[c] = deque[tail];
 	    }
 	}else{
 	    for (int i = head; i <= tail; i++){
