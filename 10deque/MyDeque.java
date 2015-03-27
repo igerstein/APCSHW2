011101f5b39c8a779b42446be55bd4b21fe5ffb2
igerstein <isaac.gerstein1@gmail.com>
2015-03-27 14:07:10 -0400

    Added toString and resize

--- 10deque/MyDeque.java
+++ 10deque/MyDeque.java
@@ -1,5 +1,17 @@
 public class MyDeque<T>{
 
+    public static void main(String[]args){
+	MyDeque<Integer> a = new MyDeque<Integer>(false);
+	a.addLast(new Integer(1));
+	a.addLast(new Integer(2));
+	a.addLast(new Integer(3));
+	System.out.println(a);
+	System.out.println(a.head);
+	System.out.println(a.tail);
+	System.out.println(a.size);
+	System.out.println(a.deque.length);
+    }
+    
     public String name(){
 	return "gerstein.isaac";
     }
@@ -7,9 +19,34 @@ public class MyDeque<T>{
     private Object[] deque;
     private int head;
     private int tail;
+    private int size;
+    private boolean shrink;
 
+    public MyDeque(boolean shrink){
+	deque = new Object[0];
+	size = 0;
+	this.shrink = shrink;
+    }
+
+    public String toString(){
+	String ans = "[ ";
+	if (tail > head && size > 0){
+	    for (int i = head; i <= tail; i++){
+		ans += deque[i] + ",";
+	    }
+	}else if (size > 0){
+	    for (int i = head; i < deque.length; i++){
+		ans += deque[i] + ",";
+	    }
+	    for (int i = 0; i <= tail; i++){
+		ans += deque[i] + ",";
+	    }
+	}
+	return ans.substring(0, ans.length() - 1) + " ]";
+    }
+    
     public void addLast(T value){
-	if (deque.length == 0){
+	if (size == 0){
 	    deque = new Object[1];
 	    deque[0] = value;
 	    head = 0;
@@ -20,14 +57,37 @@ public class MyDeque<T>{
 		tail = 0;
 	    }
 	    if (head == tail){
-		tail = deque.length;
-		resize(deque);
+		resize(size * 2);
+		head = 0;
+		tail = size;
 	    }
 	    deque[tail] = value;
 	}
+	size++;
     }
 
-    public void resize(Object[] deque){
+    public void resize(int newSize){
+	Object[] newDeque = new Object[newSize];
+	int c = 0;
+	if (tail <= head){
+	    for (int i = head; i < deque.length; i++){
+		newDeque[c] = deque[i];
+		c++;
+	    }
+	    for (int i = 0; i < tail; i++){
+		newDeque[c] = deque[i];
+		c++;
+	    }
+	    if (tail != head){
+		newDeque[c] = deque[tail];
+	    }
+	}else{
+	    for (int i = head; i <= tail; i++){
+		newDeque[c] = deque[i];
+		c++;
+	    }
+	}
+	deque = newDeque;
     }
 }
     
