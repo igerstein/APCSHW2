cfb82b4c35aa1603b74284054c15853d36e8a7f2
igerstein <isaac.gerstein1@gmail.com>
2015-04-01 13:11:42 -0400

    Added constructor and toString

--- 11BFS/Maze.java
+++ 11BFS/Maze.java
@@ -1,4 +1,11 @@
+import java.util.*;
+import java.io.*;
 public class Maze{
+    public static void main(String[]args){
+	Maze a = new Maze("data1.dat");
+	System.out.println(a);
+    }
+
     private static final String clear =  "\033[2J";
     private static final String hide =  "\033[?25l";
     private static final String show =  "\033[?25h";
@@ -7,41 +14,79 @@ public class Maze{
     }
 
     private char[][] maze;
+    private int startx, starty;
+    private int maxx, maxy;
 
     /** Same constructor as before...*/
     public Maze(String filename){
-		
+	int startx = -1;
+	int starty = -1;
+	String ans = "";
+	try{
+	    Scanner in = new Scanner(new File(filename));
+	    while(in.hasNext()){
+		String line = in.nextLine();
+		if(maxy == 0){
+		    maxx = line.length();
+		}
+		maxy++;
+		ans += line;
+	    }
+	}
+	catch(Exception e){
+	    System.out.println("File: " + filename + " could not be opened.");
+	    e.printStackTrace();
+	    System.exit(0);
+	}
+	maze = new char[maxx][maxy];
+	for(int i = 0; i < ans.length(); i++){
+	    char c = ans.charAt(i);
+	    maze[i % maxx][i / maxx] = c;
+	    if(c == 'S'){
+		startx = i % maxx;
+		starty = i / maxx;
+	    }
+	}
     }
 
-    public String toString();//do not do the funky character codes
+    public String toString(){
+	String ans = "";
+	for (int i = 0; i < maxx; i++){
+	    for (int j = 0; j < maxy; j++){
+		ans += maze[i][j] + " ";
+	    }
+	    ans += "\n";
+	}
+	return ans;
+    }
 
-    public String toString(boolean animate); //do the funky character codes when animate is true
+    //public String toString(boolean animate); //do the funky character codes when animate is true
 
     /**Solve the maze using a frontier in a BFS manner. 
      * When animate is true, print the board at each step of the algorithm.
      * Replace spaces with x's as you traverse the maze. 
      */
-    public boolean solveBFS(boolean animate){    }
+    //public boolean solveBFS(boolean animate){    }
 
     /**Solve the maze using a frontier in a DFS manner. 
      * When animate is true, print the board at each step of the algorithm.
      * Replace spaces with x's as you traverse the maze. 
      */
-    public boolean solveDFS(boolean animate){    }
+    //public boolean solveDFS(boolean animate){    }
 
-    public boolean solveBFS(){
-	return solveBFS(false);
-    }
-    public boolean solveDFS(){
-	return solveDFS(false);
-    }
+    // public boolean solveBFS(){
+    //	return solveBFS(false);
+    //  }
+    //  public boolean solveDFS(){
+    //	return solveDFS(false);
+    //  }
 
     /**return an array [x1,y1,x2,y2,x3,y3...]
      *that contains the coordinates of the solution from start to end.
      *Precondition :  solveBFS() OR solveDFS() has already been called (otherwise an empty array is returned)
      *Postcondition:  the correct solution is in the returned array
      */
-    public int[] solutionCoordinates(){
-    }
+    // public int[] solutionCoordinates(){
+    //  }
     
 }
