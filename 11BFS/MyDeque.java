ada16a94f75ea3472fce38b7732b57b2a92861ae
igerstein <isaac.gerstein1@gmail.com>
2015-04-14 20:18:57 -0400

    Changed resize and added add(value, int) and removeSmallest()

--- 11BFS/MyDeque.java
+++ 11BFS/MyDeque.java
@@ -23,6 +23,24 @@ public class MyDeque<T>{
     }
     
     public String toString(){
+	/*String ans = "[ ";
+	if (size > 0){
+	    if (tail > head){
+		for (int i = head; i <= tail; i++){
+		    ans += deque[i] + ",";
+		}
+	    }else if (tail < head){
+		for (int i = head; i < deque.length; i++){
+		    ans += deque[i] + ",";
+		}
+		for (int i = 0; i <= tail; i++){
+		    ans += deque[i] + ",";
+		}
+	    }else{
+		ans += deque[head] + ",";
+	    }
+	}
+	return ans.substring(0, ans.length() - 1) + " ]";*/
 	String ans = "[ ";
 	if (size > 0){
 	    if (tail > head){
@@ -38,6 +56,23 @@ public class MyDeque<T>{
 		}
 	    }else{
 		ans += deque[head] + ",";
+	    }
+	}
+	ans = ans.substring(0, ans.length() - 1) + " ]\n[ ";
+	if (size > 0){
+	    if (tail > head){
+		for (int i = head; i <= tail; i++){
+		    ans += priorities[i] + ",";
+		}
+	    }else if (tail < head){
+		for (int i = head; i < priorities.length; i++){
+		    ans += priorities[i] + ",";
+		}
+		for (int i = 0; i <= tail; i++){
+		    ans += priorities[i] + ",";
+		}
+	    }else{
+		ans += priorities[head] + ",";
 	    }
 	}
 	return ans.substring(0, ans.length() - 1) + " ]";
@@ -165,6 +200,66 @@ public class MyDeque<T>{
 	return element;
     }
 
+    public T removeSmallest(){
+	if (size == 0){
+	    throw new NoSuchElementException();
+	}
+	int smallest = priorities[head];
+	int indexSmallest = head;
+	if (tail < head){
+	    for (int i = head; i < priorities.length; i++){
+		if (priorities[i] < smallest){
+		    smallest = priorities[i];
+		    indexSmallest = i;
+		}
+	    }
+	    for (int i = 0; i <= tail; i++){
+		if (priorities[i] < smallest){
+		    smallest = priorities[i];
+		    indexSmallest = i;
+		}
+	    }
+	}else{
+	    for (int i = head; i <= tail; i++){
+		if (priorities[i] < smallest){
+		    smallest = priorities[i];
+		    indexSmallest = i;
+		}
+	    }
+	}
+	T element = (T)deque[indexSmallest];
+	if (tail < indexSmallest){
+	    for (int i = indexSmallest; i < deque.length; i++){
+		if (i == deque.length - 1){
+		    deque[i] = deque[0];
+		    priorities[i] = priorities[0];
+		}else{
+		    deque[i] = deque[i + 1];
+		    priorities[i] = priorities[i + 1];
+		}
+	    }
+	    for (int i = 0; i < tail; i++){
+		deque[i] = deque[i + 1];
+		priorities[i] = priorities[i + 1];
+	    }
+	}else{
+	    for (int i = indexSmallest; i < tail; i++){
+		deque[i] = deque[i + 1];
+		priorities[i] = priorities[i + 1];
+	    }
+	}
+	deque[tail] = null;
+	priorities[tail] = 0;
+	tail--;
+	size--;
+	if (size <= deque.length / 4 && size > 10 && shrink){
+	    resize(deque.length / 2);
+	    head = 0;
+	    tail = size - 1;
+	}
+	return element;
+    }
+
     public T getLast(){
 	if (size == 0){
 	    throw new NoSuchElementException();
@@ -181,23 +276,28 @@ public class MyDeque<T>{
 
     public void resize(int newSize){
 	Object[] newDeque = new Object[newSize];
+	int[] newPriorities = new int[newSize];
 	int c = 0;
 	if (tail < head){
 	    for (int i = head; i < deque.length; i++){
 		newDeque[c] = deque[i];
+		newPriorities[c] = priorities[i];
 		c++;
 	    }
 	    for (int i = 0; i <= tail; i++){
 		newDeque[c] = deque[i];
+		newPriorities[c] = priorities[i];
 		c++;
 	    }
 	}else{
 	    for (int i = head; i <= tail; i++){
 		newDeque[c] = deque[i];
+		newPriorities[c] = priorities[i];
 		c++;
 	    }
 	}
 	deque = newDeque;
+	priorities = newPriorities;
     }
 }
     
