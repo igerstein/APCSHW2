5ec01c9f275727e92d6608d84bcc7a34b75498d6
igerstein <isaac.gerstein1@gmail.com>
2015-05-12 12:51:52 -0400

    Skeleton methods

---
+++ 13Heap/MyHeap.java
@@ -0,0 +1,26 @@
+public class MyHeap{    
+    public String name(){
+	return "gerstein.isaac";
+    }
+
+    public MyHeap(){
+    }
+
+    public MyHeap(boolean isMax){
+    }
+
+    public String toString(){
+	return "";
+    }
+
+    public int remove(){
+	return 0;
+    }
+
+    public void add(int value){
+    }
+
+    public int peek(){
+	return 0;
+    }
+}
