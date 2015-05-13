71558fae25f461886077ef1bf6c2e4396a45e4e6
igerstein <isaac.gerstein1@gmail.com>
2015-05-13 16:43:37 -0400

    Finished add method and added remove and peek

--- 13Heap/MyHeap.java
+++ 13Heap/MyHeap.java
@@ -1,7 +1,13 @@
 import java.util.*;
 public class MyHeap{
     public static void main(String[]args){
-	MyHeap a = new MyHeap();
+	MyHeap a = new MyHeap(false);
+	a.add(3);
+	a.add(4);
+	a.add(5);
+	a.add(5);
+	a.add(4);
+	a.add(6);
 	System.out.println(a);
     }
 
@@ -13,12 +19,12 @@ public class MyHeap{
     }
 
     public MyHeap(){
-	heap = new int[2];
+	heap = new int[10];
 	isMax = true;
     }
 
     public MyHeap(boolean isMax){
-	heap = new int[2];
+	heap = new int[10];
 	this.isMax = isMax;
     }
 
@@ -27,17 +33,75 @@ public class MyHeap{
     }
 
     public int remove(){
-	return 0;
+	if (heap[0] == 0){
+	    throw new IndexOutOfBoundsException();
+	}
+	int root = heap[1];
+	heap[1] = heap[heap[0]];
+	heap[heap[0]] = 0;
+	heap[0]--;
+	int pos = 1;
+	if (isMax){
+	    while ((pos * 2 <= heap[0] && heap[pos] < heap[pos * 2]) || (pos * 2 + 1 <= heap[0] && heap[pos] < heap[pos * 2 + 1])){
+		if (heap[pos] < heap[pos * 2]){
+		    int temp = heap[pos * 2];
+		    heap[pos * 2] = heap[pos];
+		    heap[pos] = temp;
+		    pos *= 2;
+		}else{
+		    int temp = heap[pos * 2 + 1];
+		    heap[pos * 2 + 1] = heap[pos];
+		    heap[pos] = temp;
+		    pos = pos * 2 + 1;
+		}
+	    }
+	}else{
+	    while ((pos * 2 <= heap[0] && heap[pos] > heap[pos * 2]) || (pos * 2 + 1 <= heap[0] && heap[pos] > heap[pos * 2 + 1])){
+		if (heap[pos] > heap[pos * 2]){
+		    int temp = heap[pos * 2];
+		    heap[pos * 2] = heap[pos];
+		    heap[pos] = temp;
+		    pos *= 2;
+		}else{
+		    int temp = heap[pos * 2 + 1];
+		    heap[pos * 2 + 1] = heap[pos];
+		    heap[pos] = temp;
+		    pos = pos * 2 + 1;
+		}
+	    }
+	}
+	return root;
     }
 
     public void add(int value){
 	if (heap.length - 1 == heap[0]){
 	    resize(heap.length * 2 - 1);
 	}
+	heap[0]++;
+	int pos = heap[0];
+	heap[pos] = value;
+	if (isMax){
+	    while (heap[pos] > heap[pos / 2] && pos / 2 > 0){
+		int temp = heap[pos / 2];
+		heap[pos / 2] = heap[pos];
+		heap[pos] = temp;
+		pos /= 2;
+	    }
+	}else{
+	    while (heap[pos] < heap[pos / 2] && pos / 2 > 0){
+		int temp = heap[pos / 2];
+		heap[pos / 2] = heap[pos];
+		heap[pos] = temp;
+		pos /= 2;
+	    }
+	}
     }
 
     public int peek(){
-	return 0;
+	if (heap[0] == 0){
+	    throw new IndexOutOfBoundsException();
+	}
+	return heap[1];
     }
 
     public void resize(int newSize){
